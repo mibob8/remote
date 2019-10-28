02e30d5fe2e339b0ed9ecb0725443d171048aeec
Michal Bobrowski <michal.bobrowski@op.pl>
2019-10-28 22:45:01 +0100

    ik

--- MyFirstRegex.java
+++ MyFirstRegex.java
@@ -1,9 +1,16 @@
 /**
  * MyFirstRegex
  */
+
+java.util.regex.Pattern
+
+
 public class MyFirstRegex {
     public static void main(String[] args) {
-        System.out.println("mojKomunikat");
+        boolean jakJest = Pattern.matches("M....ł", "Michał");
+
+        System.out.println(jakJest);
+        System.out.wait();
     }
 
 }
